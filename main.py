c9358858c4d53c64ec352ef793f02fe84715ab0e
Tim-Harrington <tjh5415@gmail.com>
2022-04-25 02:45:57 +0000

    Adding bigQuery query
    Adding a new query to test bigquery while also adding new packages to requirements.txt

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 import os
 
 from flask import Flask
+from flask_restful import Api
+from google.cloud import bigquery
 
+
+#Borrowing logic from Erin McMahon's code from week 3 demo discussion
 app = Flask(__name__)
+api = App(app)
 
+client = bigquery.Client()
 
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
     return "Hello {}!".format(name)
+
+@app.route("/")
+def query_data():
+    query = """
+    SELECT *
+    FROM bigquery-public-data.world_bank_health_population
+    LIMIT 10
+    """
+    query_job = client.query(query)
+
+    df = query_job.to_dataframe()
+    json_object = df.to_json(orient='records')
+
+    return json_object
 
 
 if __name__ == "__main__":
